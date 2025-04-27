2e6cf1285ee4f667a1d9497200b7a33d523accd8
smartsimon <smartsimon@git.com>
2025-04-27 19:29:24 +0300

    init commit

---
+++ client/src/test/java/org/smart/simon/demo/grpc/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.smart.simon.demo.grpc.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ server/src/main/java/org/smart/simon/demo/grpc/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package org.smart.simon.demo.grpc.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
