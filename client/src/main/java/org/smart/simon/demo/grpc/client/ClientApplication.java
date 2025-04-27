79ed903f6aef12f85191c7dd92fb59f6650fc711
smartsimon <smartsimon@git.com>
2025-04-27 23:41:48 +0300

    simple gRPC example

--- client/src/main/java/org/smart/simon/demo/grpc/client/ClientApplication.java
+++ client/src/main/java/org/smart/simon/demo/grpc/client/ClientApplication.java
@@ -1,13 +1,26 @@
 package org.smart.simon.demo.grpc.client;
 
+import org.smart.simon.demo.grpc.client.service.GrpcClientService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class ClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ClientApplication.class, args);
+        ApplicationContext context = SpringApplication.run(ClientApplication.class, args);
+        GrpcClientService service = context.getBean(GrpcClientService.class);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String name = scanner.nextLine();
+            String text = scanner.nextLine();
+            String message = service.sayHello(name, text);
+            System.out.println(message);
+        }
     }
 
 }
